00569c5b2b1e51c1a758739b7b1113dfe3fa6d7c
akursanova <kursandra@gmail.com>
2014-04-22 02:03:17 +0400

    up

--- src/com/example/fw/ApplicationManager.java
+++ src/com/example/fw/ApplicationManager.java
@@ -1,17 +1,25 @@
 package com.example.fw;
 
+import java.io.IOException;
 import java.util.Properties;
 
 public class ApplicationManager {
 	
+	protected final ApplicationManager manager;
 	private Properties properties;
+	private Process process;
 	
 	public  ApplicationManager(Properties properties) {
 		this.properties = properties;
 	}
 
 	public void stop() {
-		
+		process.destroy();
+	}
+	
+	public void start() throws IOException {
+		String command = manager.getProperty("app.path");
+		process = Runtime.getRuntime().exec(command);
 	}
 
 	public void setProperties(Properties properties){
--- src/com/example/tests/TestBase.java
+++ src/com/example/tests/TestBase.java
@@ -1,25 +1,12 @@
 package com.example.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Properties;
-import java.util.Random;
-
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
-
 import com.example.fw.ApplicationManager;
-import com.sun.istack.internal.logging.Logger;
+
 
 public class TestBase {
 
